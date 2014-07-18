8940f8bfc241597c6cecf5db05095207e17e43eb
3dfxsoftware <cbss@3dfx.biz>
2014-07-17 20:44:18 -0500

    updates

--- odoo_install.sh
+++ odoo_install.sh
@@ -71,7 +71,7 @@ sudo chown $OE_USER:$OE_USER /var/log/$OE_USER
 # Install ODOO
 #--------------------------------------------------
 echo -e "\n==== Installing ODOO Server ===="
-sudo git clone N https://www.github.com/odoo/odoo $OE_HOME_EXT/
+sudo git clone https://github.com/3dfxsoftware/odoo.git $OE_HOME_EXT/
 
 echo -e "\n---- Create custom module directory ----"
 sudo su $OE_USER -c "mkdir $OE_HOME/custom"
